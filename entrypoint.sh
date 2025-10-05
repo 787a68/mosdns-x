79da279774d4ffebad55358b14e05739d20f84e3
787a68 <36852208+787a68@users.noreply.github.com>
2025-10-05 22:12:27 +0800

    增加仅挂载配置文件方案

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,19 +13,25 @@ if [ -z "$(ls -A $CONFIG_DIR 2>/dev/null)" ]; then
     cp -rT "$DEFAULT_CONFIG_DIR/" "$CONFIG_DIR/"
     echo "Default configuration copied."
 fi
+# 将挂载的 `/data/config.yml` 文件复制并覆盖到 /etc/mosdns
+if [ -f "/data/config.yaml" ]; then
+    echo "Found custom config at /data/config.yml. Copying to $CONFIG_DIR..."
+    # 复制并覆盖默认配置
+    cp -fv /data/config.yml "$CONFIG_DIR/"
+fi
 
 CRONTAB_FILE="/etc/crontabs/root"
 # 动态生成 crontab 配置文件
 # 将日志输出重定向到容器的标准输出流，方便通过 docker logs 查看
 echo "Initializing cron jobs..."
 if [ -n "$crontab" ]; then
-    # 直接执行 /easymosdns/rules/中的原始 update 脚本
+    # 执行 /easymosdns/rules/中的 update 脚本
     echo "$crontab $DEFAULT_CONFIG_DIR/rules/update >> /proc/1/fd/1 2>&1" >> "$CRONTAB_FILE"
     echo "  -> Scheduled direct update: '$crontab'. Using pristine script."
 fi
 
 if [ -n "$crontabcnd" ]; then
-    # 直接执行 /easymosdns/rules/中的原始 update-cdn 脚本
+    # 执行 /easymosdns/rules/中的 update-cdn 脚本
     echo "$crontabcnd $DEFAULT_CONFIG_DIR/rules/update-cdn >> /proc/1/fd/1 2>&1" >> "$CRONTAB_FILE"
     echo "  -> Scheduled CDN update: '$crontabcnd'. Using pristine script."
 fi
